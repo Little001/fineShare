4c40510313569e80f5c6c7eb3d82d0e85927bd96
Martin <martin@localhost.localdomain>
2016-02-12 08:03:57 +0100

    my first commit

---
+++ JS/Scripts.js
@@ -0,0 +1,32 @@
+$( document ).ready(function() {
+	/*Při kliknutí na filter "Vše, Video, Audio..."*/
+	$(".searchUploadComponent ul li").click(function() {
+        /*Grafické upravy*/
+        $(".searchUploadComponent ul li").each(function() {
+            console.log($(this).removeClass("active"));
+        });
+        $(this).addClass("active");
+	});
+    
+	/*Při kliknutí na hledat*/
+	$("#searchButton").click(function() {
+		startLoading();
+		setTimeout(stopLoading, 2000);
+	});	
+    
+    /*Při stlačení tlačítka, pro najetí nahoru stránky*/
+    $(".buttonUpScroll").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+});
+
+
+function startLoading(){
+  $('.loader').css('display', 'block');
+}
+
+function stopLoading(){
+  $('.loader').css('display', 'none');
+}
+
+
